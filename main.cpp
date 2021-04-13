bff0a86a7c1361418c63512d820e4dea14cb1f1b
Nickytt <toglokhinov.nb@phystech.edu>
2021-04-14 00:54:33 +0300

    Revamp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 #include <map>
-#include <set>
 #include <iterator>
 
 class Table {
 protected:
     std::map<std::string, int> table;
-    std::set<int> marks;
+    std::multimap<int, std::string> marks;
 public:
     Table() {};
     void insert(std::string name, int score) {
         table[name] = score;
-        marks.insert(score);
+        marks.insert(std::pair<int, std::string>(score, name));
     }
     void list_students() {
         std::cout << std::endl << "LIST OF STUDENTS:" << std::endl;
@@ -19,34 +18,28 @@ public:
         std::cout << std::endl;
     }
     void find(std::string name) {
-        auto x = std::find_if(std::begin(table), std::end(table), [&name](auto x){return x.first == name;});
         try{
-            if(x != table.end()) {
-                std::cout << "Mark of " << name << " is " << x->second << std::endl;
-            } else {
-                throw std::invalid_argument("This student is not found");
-            }
+            auto x = table.at(name);
+            std::cout << "The score of " << name << " is " << x << std::endl;
         } catch(std::exception& ex) {
-            std::cout << ex.what() << std::endl;
+            std::cout << "This student is not found" << std::endl;
         }
     }
-    unsigned int range(int x) {
+    void range(int x) {
+        std::cout << std::endl << "Students with " << x << " points:" << std::endl;
         unsigned int res = 0;
         std::for_each(std::begin(table), std::end(table), [&res, &x](auto u){
             if(u.second == x) {
+                std::cout << u.first << std::endl;
                 res++;
             }
         });
-        return res;
+        std::cout << "(" << res << " students)" << std::endl;
     }
     void standings() {
         std::cout << std::endl << "STANDINGS:" << std::endl;
         for(auto it = std::rbegin(marks); it != std::rend(marks); it++) {
-            std::for_each(std::begin(table), std::end(table), [&it](auto x){
-                if(x.second == *it) {
-                    std::cout << x.first << ":\t" << x.second << std::endl;
-                }
-            });
+            std::cout << it->second << ":\t" << it->first << std::endl;
         }
         std::cout << std::endl;
     }
@@ -57,14 +50,14 @@ int main() {
     a.insert("Nalitch Peter Andreevich", 100);
     a.insert("Bond Ira Sergeevna", 72);
     a.insert("Bondareva Arina Semenovna", 72);
-    a.insert("Zaribin Mihail Petrovich", 81);
+    a.insert("Zarubin Mihail Petrovich", 81);
     a.insert("Malich Artem Evgenyevich", 95);
     a.list_students();
     a.find("Malich Artem Evgenyevich");
     a.find("Lee Amy Lynn");
     a.insert("Malich Artem Evgenyevich", 98);
-    std::cout << a.range(72) << std::endl;
-    std::cout << a.range(99) << std::endl;
+    a.range(72);
+    a.range(99);
     a.standings();
     return 0;
 }
